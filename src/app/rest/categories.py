00b33fa8b4b1476dcf6922dfae71e7d0c5dcbc53
Elad Gunders <gunderselad@gmail.com>
2023-04-17 14:18:10 +0300

    ✨ added categories endpoint

---
+++ src/app/rest/categories.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, status, Depends
+
+from db.user import current_logged_user
+from app.db.db_facade import DBFacade
+from app.db.types.category import CategoryType
+
+router = APIRouter(
+    prefix='/categories',
+    dependencies=[Depends(current_logged_user)],
+    tags=['Categories']
+)
+
+db_facade = DBFacade.get_instance()
+
+
+@router.get('', status_code=status.HTTP_200_OK)
+async def get_categories(user=Depends(current_logged_user)) -> list[CategoryType]:
+    return await db_facade.get_categories(user.id)
