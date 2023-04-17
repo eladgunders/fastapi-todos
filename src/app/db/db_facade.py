ee669d095a0d76ee1df3361c4288ba414a2b47f4
Elad Gunders <gunderselad@gmail.com>
2023-04-17 09:14:33 +0300

    🔨 refactored public to private properties

--- src/app/db/db_facade.py
+++ src/app/db/db_facade.py
@@ -5,24 +5,24 @@ from config import get_config
 
 
 class DBFacade:
-    instance = None
+    _instance = None
     _lock = threading.Lock()
-    repo: SQLManager
+    _repo: SQLManager
 
     def __init__(self):
         raise RuntimeError('Call get_instance() instead')
 
     @classmethod
     def get_instance(cls):
-        if cls.instance:
-            return cls.instance
+        if cls._instance:
+            return cls._instance
         with cls._lock:
-            if cls.instance is None:
-                cls.instance = cls.__new__(cls)
-                cls.instance.config = get_config()
-                cls.instance.repo = SQLManager()
-                cls.instance.repo.connect_to_database(cls.instance.config.db_conn_str)
-            return cls.instance
+            if cls._instance is None:
+                cls._instance = cls.__new__(cls)
+                cls._instance.config = get_config()
+                cls._instance.repo = SQLManager()
+                cls._instance.repo.connect_to_database(cls._instance.config.db_conn_str)
+            return cls._instance
 
     async def disconnect_from_databases(self) -> None:
-        await self.repo.close_database_connection()
+        await self._repo.close_database_connection()
--- src/app/db/sql_manager.py
+++ src/app/db/sql_manager.py
@@ -5,19 +5,19 @@ from sqlalchemy.orm import sessionmaker
 
 
 class SQLManager:
-    engine: AsyncEngine
-    local_session: async_scoped_session
+    _engine: AsyncEngine
+    _local_session: async_scoped_session
 
     def connect_to_database(self, conn_str: str) -> None:
         print('INFO:     connecting to db.')
-        self.engine = create_async_engine(conn_str, echo=True)
-        async_session_factory = sessionmaker(bind=self.engine, class_=AsyncSession,
+        self._engine = create_async_engine(conn_str, echo=True)
+        async_session_factory = sessionmaker(bind=self._engine, class_=AsyncSession,
                                              expire_on_commit=False)
-        self.local_session = async_scoped_session(async_session_factory, scopefunc=current_task)
+        self._local_session = async_scoped_session(async_session_factory, scopefunc=current_task)
         print('INFO:     connected to db.')
 
     async def close_database_connection(self) -> None:
         print('INFO:     closing connection with db.')
-        await self.local_session().close()
-        await self.engine.dispose()
+        await self._local_session().close()
+        await self._engine.dispose()
         print('INFO:     closed connection with db.')
