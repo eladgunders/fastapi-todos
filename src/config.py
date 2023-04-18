83bb245e8e4997c9d68e25174d8dc048cfafc682
Elad Gunders <gunderselad@gmail.com>
2023-04-18 18:36:35 +0300

    🔥 removed unnecessary code

--- src/config.py
+++ src/config.py
@@ -1,29 +1,17 @@
-import os
 from functools import lru_cache
-from dotenv import load_dotenv
 from pydantic import BaseSettings
 
-from app.constants.auth import JWT_TOKEN_LIFETIME_SECONDS
 
-
-class Config(BaseSettings):
+class Settings(BaseSettings):
     db_conn_str: str
     origins: str
     jwt_secret_key: str
     jwt_token_lifetime_seconds: int
 
     class Config:
-        load_dotenv()
-
-        db_conn_str: str = os.environ['DB_CONN_STR']
-        origins: list[str] = os.environ['ORIGINS'].split(';')
-        jwt_secret_key: str = os.environ['JWT_SECRET_KEY']
-        try:
-            jwt_token_lifetime_seconds = int(os.getenv('JWT_TOKEN_LIFETIME_SECONDS', str(JWT_TOKEN_LIFETIME_SECONDS)))
-        except ValueError:
-            jwt_token_lifetime_seconds = JWT_TOKEN_LIFETIME_SECONDS
+        env_file = '.env'
 
 
 @lru_cache()
 def get_config():
-    return Config()
+    return Settings()
