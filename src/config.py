655d788b7ca549e882c02fa549c3543720a7f1f3
Elad Gunders <gunderselad@gmail.com>
2023-04-18 18:47:06 +0300

    🐛 fixed origins type

--- src/config.py
+++ src/config.py
@@ -1,17 +1,26 @@
 from functools import lru_cache
 from pydantic import BaseSettings
+from typing import Any
 
 
 class Settings(BaseSettings):
     db_conn_str: str
-    origins: str
+    origins: list[str]
     jwt_secret_key: str
     jwt_token_lifetime_seconds: int
 
     class Config:
         env_file = '.env'
 
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str) -> Any:
+            if field_name == 'origins':
+                return [origin for origin in raw_val.split(';')]
+            return cls.json_loads(raw_val)
+
 
 @lru_cache()
 def get_config():
-    return Settings()
+    s = Settings()
+    print(s.dict())
+    return s
