1a299d60b9fa3cdd869c00cad012e4858eec2915
Elad Gunders <gunderselad@gmail.com>
2023-04-24 09:15:05 +0300

    ✨ added add todo

--- src/app/utils/exceptions.py
+++ src/app/utils/exceptions.py
@@ -7,6 +7,10 @@ class ResourceNotExists(Exception):
     pass
 
 
+class Forbidden(Exception):
+    pass
+
+
 def exception_handler(f: Callable) -> Any:
     @wraps(f)
     async def decorated(*args, **kwargs):
@@ -16,4 +20,6 @@ def exception_handler(f: Callable) -> Any:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
         except ResourceNotExists as err:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(err))
+        except Forbidden as err:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(err))
     return decorated
