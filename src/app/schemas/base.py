baf540c7fa896932c1ef71858d8cf2c7a4ad4efa
Elad Gunders <gunderselad@gmail.com>
2023-04-25 22:42:32 +0300

    👕 fixed lint

--- src/app/schemas/base.py
+++ src/app/schemas/base.py
@@ -11,4 +11,4 @@ class BaseInDB(BaseModel):
     def to_orm(self):
         if not self.Config.orm_model:
             raise AttributeError("Class has not defined Config.orm_model")
-        return self.Config.orm_model(**dict(self))
+        return self.Config.orm_model(**dict(self))  # pylint: disable=not-callable
