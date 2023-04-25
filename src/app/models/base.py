eb8173fb847ca48fa35a404fa58582c7d134c365
Elad Gunders <gunderselad@gmail.com>
2023-04-25 21:28:07 +0300

    💄 renamed function

--- src/app/models/base.py
+++ src/app/models/base.py
@@ -12,11 +12,11 @@ class Base:
     def __tablename__(cls) -> str:  # pylint: disable=no-self-argument
         return humps.depascalize(cls.__name__)
 
-    def get_dict(self) -> dict[str, Any]:
+    def dict(self) -> dict[str, Any]:
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
 
     def __repr__(self) -> str:
-        columns = [f'{col}: {getattr(self, col)}' for col in self.get_dict()]
+        columns = [f'{col}: {getattr(self, col)}' for col in self.dict()]
         return f'{self.__class__.__name__}({", ".join(columns)})'
 
     def __str__(self) -> str:
