e9c3fbe1ae30ad9f307a128fa9f2643226739f89
Elad Gunders <gunderselad@gmail.com>
2023-04-26 15:25:00 +0300

    💄 renamed Forbidden to UserNotAllowed

--- src/app/dao/db_facade.py
+++ src/app/dao/db_facade.py
@@ -7,7 +7,7 @@ from app.dao.sql_manager import SQLManager
 from app.models.tables import Priority, Category, Todo
 from app.schemas import CategoryInDB, TodoInDB
 from app.core.config import get_config
-from app.utils.exceptions import ResourceNotExists, Forbidden, ResourceAlreadyExists
+from app.utils.exceptions import ResourceNotExists, UserNotAllowed, ResourceAlreadyExists
 
 
 class DBFacade:
@@ -58,7 +58,7 @@ class DBFacade:
         if not category:
             raise ResourceNotExists(resource='category')
         if category.created_by_id != created_by_id:
-            raise Forbidden('a user can not delete a category that was not created by him')
+            raise UserNotAllowed('a user can not delete a category that was not created by him')
         await self._repo.delete_category(category_id)
 
     async def get_todos(self, created_by_id: uuid.UUID) -> list[Todo]:
--- src/app/utils/exceptions.py
+++ src/app/utils/exceptions.py
@@ -9,7 +9,7 @@ class ResourceNotExists(Exception):
         super().__init__(self.msg)
 
 
-class Forbidden(Exception):
+class UserNotAllowed(Exception):
     pass
 
 
@@ -28,7 +28,7 @@ def exception_handler(f: Callable) -> Any:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
         except ResourceNotExists as err:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(err))
-        except Forbidden as err:
+        except UserNotAllowed as err:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(err))
         except ResourceAlreadyExists as err:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(err))
