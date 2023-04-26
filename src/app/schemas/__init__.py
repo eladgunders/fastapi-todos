5e5c0f22bc97b4b26d86a69dc8998cca0b161ac2
Elad Gunders <gunderselad@gmail.com>
2023-04-26 13:19:41 +0300

    📚 added documentation

--- src/app/schemas/__init__.py
+++ src/app/schemas/__init__.py
@@ -1,3 +1,9 @@
+"""
+Read - properties to return to client
+Create - properties to receive on item creation
+Update - properties to receive on item update
+InDB -  properties stored in DB
+"""
 from .priority import PriorityRead
 from .category import CategoryRead, CategoryCreate, CategoryInDB
 from .todo import TodoRead, TodoCreate, TodoInDB
