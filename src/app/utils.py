a60ef82e9fe2929421a977ec3495c0ea59c296a5
Elad Gunders <gunderselad@gmail.com>
2023-04-27 14:57:10 +0300

    🔨 refactored exception handler

--- src/app/utils.py
+++ src/app/utils.py
@@ -1,4 +1,4 @@
-from typing import Callable, Any
+from typing import Callable, Any, Type
 from functools import wraps
 
 from fastapi import status, HTTPException
@@ -7,16 +7,21 @@ from app.http_exceptions import ResourceNotExists, UserNotAllowed, ResourceAlrea
 
 
 def exception_handler(f: Callable) -> Any:
+    exception_map: dict[Type[Exception], status] = {
+        ValueError: status.HTTP_400_BAD_REQUEST,
+        UserNotAllowed: status.HTTP_403_FORBIDDEN,
+        ResourceNotExists: status.HTTP_404_NOT_FOUND,
+        ResourceAlreadyExists: status.HTTP_409_CONFLICT,
+    }
+
+    exceptions: tuple[Type[Exception]] = tuple(exception_map.keys())
+
     @wraps(f)
     async def decorated(*args, **kwargs):
         try:
             return await f(*args, **kwargs)
-        except ValueError as err:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
-        except UserNotAllowed as err:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(err))
-        except ResourceNotExists as err:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(err))
-        except ResourceAlreadyExists as err:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(err))
+        except exceptions as err:
+            exception_cls = type(err)
+            status_code = exception_map[exception_cls]
+            raise HTTPException(status_code=status_code, detail=str(err))
     return decorated
