f6c1ee1da9306de214e3dcdd7c32389b52804c30
Elad Gunders <gunderselad@gmail.com>
2023-04-28 12:43:22 +0300

    🎨 added a session dependency in the api endpoints

--- src/app/api/categories.py
+++ src/app/api/categories.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.auth.deps import get_async_session
 from app.api.deps import current_logged_user
 from app.dao.db_facade import DBFacade
 from app.schemas import CategoryCreate, CategoryRead, CategoryInDB
@@ -7,7 +9,10 @@ from app.utils import exception_handler
 
 router = APIRouter(
     prefix='/categories',
-    dependencies=[Depends(current_logged_user)],
+    dependencies=[
+        Depends(current_logged_user),
+        Depends(get_async_session)
+    ],
     tags=['Categories']
 )
 
@@ -15,19 +20,30 @@ db_facade = DBFacade.get_instance()
 
 
 @router.get('', response_model=list[CategoryRead])
-async def get_categories(user=Depends(current_logged_user)):
+async def get_categories(
+    session: AsyncSession = Depends(get_async_session),
+    user=Depends(current_logged_user)
+):
     return await db_facade.get_categories(user.id)
 
 
 @router.post('', response_model=CategoryRead, status_code=status.HTTP_201_CREATED)
 @exception_handler
-async def add_category(category_in: CategoryCreate, user=Depends(current_logged_user)):
+async def add_category(
+    category_in: CategoryCreate,
+    session: AsyncSession = Depends(get_async_session),
+    user=Depends(current_logged_user)
+):
     category = CategoryInDB(name=category_in.name, created_by_id=user.id)
     return await db_facade.add_category(category)
 
 
 @router.delete('/{category_id}', status_code=status.HTTP_204_NO_CONTENT)
 @exception_handler
-async def delete_category(category_id: int, user=Depends(current_logged_user)):
+async def delete_category(
+    category_id: int,
+    session: AsyncSession = Depends(get_async_session),
+    user=Depends(current_logged_user)
+):
     await db_facade.delete_category(category_id, user.id)
 
--- src/app/api/todos.py
+++ src/app/api/todos.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.auth.deps import get_async_session
 from app.api.deps import current_logged_user
 from app.dao.db_facade import DBFacade
 from app.schemas import TodoRead, TodoInDB, TodoCreate
@@ -8,7 +10,10 @@ from app.utils import exception_handler
 
 router = APIRouter(
     prefix='/todos',
-    dependencies=[Depends(current_logged_user)],
+    dependencies=[
+        Depends(current_logged_user),
+        Depends(get_async_session)
+    ],
     tags=['Todos']
 )
 
@@ -16,13 +21,20 @@ db_facade = DBFacade.get_instance()
 
 
 @router.get('', response_model=list[TodoRead])
-async def get_todos(user=Depends(current_logged_user)):
+async def get_todos(
+    session: AsyncSession = Depends(get_async_session),
+    user=Depends(current_logged_user)
+):
     return await db_facade.get_todos(user.id)
 
 
 @router.post('', response_model=TodoRead, status_code=status.HTTP_201_CREATED)
 @exception_handler
-async def add_todo(todo_in: TodoCreate, user=Depends(current_logged_user)):
+async def add_todo(
+    todo_in: TodoCreate,
+    session: AsyncSession = Depends(get_async_session),
+    user=Depends(current_logged_user)
+):
     todo = TodoInDB(
         content=todo_in.content,
         priority_id=todo_in.priority_id,
@@ -34,5 +46,9 @@ async def add_todo(todo_in: TodoCreate, user=Depends(current_logged_user)):
 
 @router.delete('/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
 @exception_handler
-async def delete_todo(todo_id: int, user=Depends(current_logged_user)):
+async def delete_todo(
+    todo_id: int,
+    session: AsyncSession = Depends(get_async_session),
+    user=Depends(current_logged_user)
+):
     await db_facade.delete_todo(todo_id, user.id)
