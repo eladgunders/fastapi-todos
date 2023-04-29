c75f90573401de47c485539b16d44da06ef6653e
Elad Gunders <gunderselad@gmail.com>
2023-04-30 01:30:57 +0300

    trying to fix ci

--- src/scripts/initiate_data.py
+++ src/scripts/initiate_data.py
@@ -32,7 +32,7 @@ get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
 
 async def create_user(email: str, password: str = None) -> None:
     try:
-        async with get_async_session_context(conn) as session:
+        async with get_async_session_context() as session:
             async with get_user_db_context(session) as user_db:
                 async with get_user_manager_context(user_db) as user_manager:
                     await user_manager.create(
