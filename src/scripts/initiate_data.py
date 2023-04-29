f3047b2ed1eaa3b4a6c08bbe0e4089d930823e40
Elad Gunders <gunderselad@gmail.com>
2023-04-30 01:30:33 +0300

    trying to fix ci

--- src/scripts/initiate_data.py
+++ src/scripts/initiate_data.py
@@ -5,32 +5,26 @@ from sqlalchemy.ext.asyncio import AsyncSession, AsyncConnection
 from fastapi_users.db import SQLAlchemyUserDatabase
 from fastapi_users.exceptions import UserAlreadyExists
 
-from app.core.db import engine
+from app.core.db import Session
 from app.core.security import UserManager
 from app.models.tables import User
+from app.api.auth.deps import get_async_session, get_user_db, get_user_manager
 from app.schemas import UserCreate
 
 
-async def get_connection():
-    async with engine.begin() as conn:
-        yield conn
-        await conn.rollback()
+# async def get_async_session():
+#     async with Session() as async_session_:
+#         yield async_session_
+#
+#
+# async def get_user_db(async_session: AsyncSession):
+#     yield SQLAlchemyUserDatabase(async_session, User)
+#
+#
+# async def get_user_manager(user_db: SQLAlchemyUserDatabase):
+#     yield UserManager(user_db)
 
 
-async def get_async_session(connection: AsyncConnection):
-    async with AsyncSession(connection, expire_on_commit=False) as async_session_:
-        yield async_session_
-
-
-async def get_user_db(async_session: AsyncSession):
-    yield SQLAlchemyUserDatabase(async_session, User)
-
-
-async def get_user_manager(user_db: SQLAlchemyUserDatabase):
-    yield UserManager(user_db)
-
-
-get_async_connection_context = contextlib.asynccontextmanager(get_connection)
 get_async_session_context = contextlib.asynccontextmanager(get_async_session)
 get_user_db_context = contextlib.asynccontextmanager(get_user_db)
 get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
@@ -38,13 +32,12 @@ get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
 
 async def create_user(email: str, password: str = None) -> None:
     try:
-        async with get_async_connection_context() as conn:
-            async with get_async_session_context(conn) as session:
-                async with get_user_db_context(session) as user_db:
-                    async with get_user_manager_context(user_db) as user_manager:
-                        await user_manager.create(
-                            UserCreate(email=email, password=password)
-                        )
+        async with get_async_session_context(conn) as session:
+            async with get_user_db_context(session) as user_db:
+                async with get_user_manager_context(user_db) as user_manager:
+                    await user_manager.create(
+                        UserCreate(email=email, password=password)
+                    )
     except UserAlreadyExists:
         print('User with {user_name} mail address already exists')
 
