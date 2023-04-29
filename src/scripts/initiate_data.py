eaaa1393da31e3ae60108e06f621080e3db91e06
Elad Gunders <gunderselad@gmail.com>
2023-04-30 00:35:28 +0300

    trying to fix ci

--- src/scripts/initiate_data.py
+++ src/scripts/initiate_data.py
@@ -18,16 +18,16 @@ async def get_connection():
         await conn.rollback()
 
 
-async def get_async_session(connection: AsyncConnection = get_connection()):
+async def get_async_session(connection: AsyncConnection):
     async with AsyncSession(connection, expire_on_commit=False) as async_session_:
         yield async_session_
 
 
-async def get_user_db(async_session: AsyncSession = get_async_session()):
+async def get_user_db(async_session: AsyncSession):
     yield SQLAlchemyUserDatabase(async_session, User)
 
 
-async def get_user_manager(user_db: SQLAlchemyUserDatabase = get_user_db()):
+async def get_user_manager(user_db: SQLAlchemyUserDatabase):
     yield UserManager(user_db)
 
 
@@ -38,7 +38,7 @@ get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
 
 async def create_user(user_name: str, password: str = None) -> None:
     try:
-        async with get_async_session_context() as session:
+        async with get_async_session_context(get_connection()) as session:
             async with get_user_db_context(session) as user_db:
                 async with get_user_manager_context(user_db) as user_manager:
                     # auto generate password
