94bf7126d089afa610f7cce6c6ac1f37c4e6b43b
Elad Gunders <gunderselad@gmail.com>
2023-04-29 23:54:34 +0300

    trying to fix ci

--- src/scripts/init_db.py
+++ src/scripts/init_db.py
@@ -4,5 +4,4 @@ from app.core.db import create_all_entities
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(create_all_entities())
+    asyncio.run(create_all_entities())
