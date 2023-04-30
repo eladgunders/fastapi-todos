2a3a0e06334f62b155e5566d096ec8d47f6c12a7
Elad Gunders <gunderselad@gmail.com>
2023-04-30 14:34:36 +0300

    fix ci

--- src/__tests__/test_priorities.py
+++ src/__tests__/test_priorities.py
@@ -11,7 +11,7 @@ async def test_priorities(client: AsyncClient, user_token_headers: dict[str, str
     assert res_without_credentials.status_code == 401
     assert res_without_credentials.json() == {'detail': 'Unauthorized'}
     res_with_credentials = await client.get('/priorities', headers=user_token_headers)
-    with open('app/scripts/initial_data.json', 'r') as f:
+    with open('scripts/initial_data.json', 'r') as f:
         initial_data_dict: dict[str, list[dict]] = json.load(f)
         priorities = initial_data_dict['priorities']
     assert res_with_credentials.status_code == 200
