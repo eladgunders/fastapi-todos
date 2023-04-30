27b39a107847f245b3d617068437607d202e170c
Elad Gunders <gunderselad@gmail.com>
2023-04-30 15:07:01 +0300

    improved structure

--- src/__tests__/constants.py
+++ src/__tests__/constants.py
@@ -4,4 +4,4 @@ from pydantic import EmailStr
 
 TEST_USER_EMAIL: Final[EmailStr] = EmailStr('test@test.com')
 TEST_USER_PASSWORD: Final[str] = 'test'
-INITIAL_DATA_FILE_PATH: Final[str] = 'src/__tests__/scripts/initial_data.json'
+INITIAL_DATA_FILE_PATH: Final[str] = 'src/scripts/initial_data.json'
