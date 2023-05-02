b33427cdd03758ba3023548a2def651e85f5d5fb
Elad Gunders <gunderselad@gmail.com>
2023-05-02 20:23:09 +0300

    💄📚 renamed var + documentation

--- todos/app/dal/__init__.py
+++ todos/app/dal/__init__.py
@@ -1,2 +1,2 @@
 from .db_service import db_service
-from .constants import MAX_LIMIT_GET_MULTI, GET_MULTI_DEFAULT_SKIP, GET_MULTI_DEFAULT_LIMIT
+from .constants import MAX_POSTGRES_INTEGER, GET_MULTI_DEFAULT_SKIP, GET_MULTI_DEFAULT_LIMIT
--- todos/app/dal/constants.py
+++ todos/app/dal/constants.py
@@ -1,10 +1,11 @@
 from typing import Final
 
 
-MAX_LIMIT_GET_MULTI: Final[int] = (2 ** 31) - 1
+MAX_POSTGRES_INTEGER: Final[int] = (2 ** 31) - 1
 """
 maximum value that can be represented by a 32-bit signed integer.
-it is the postgresql maximum get multi query limit == no limit.
+if trying to send a bigger value than that in queries (like offset or limit)
+the database throws an error - OverflowError: value out of int32 range
 """
 GET_MULTI_DEFAULT_SKIP: Final[int] = 0
 GET_MULTI_DEFAULT_LIMIT: Final[int] = 100
