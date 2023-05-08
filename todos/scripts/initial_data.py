04303beecc026feb9c1bc7768b534f394824d59d
Elad Gunders <gunderselad@gmail.com>
2023-05-08 10:38:21 +0300

    🔨 refactored structure of the initial data file and added console logs

--- todos/scripts/initial_data.py
+++ todos/scripts/initial_data.py
@@ -1,11 +1,17 @@
 from typing import Final
 import json
+import logging
 
 import asyncio
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import Session
 from app.models.tables import Priority, Category
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 INITIAL_DATA_FILE_PATH: Final[str] = 'todos/scripts/initial_data.json'
@@ -13,21 +19,39 @@ INITIAL_DATA_FILE_PATH: Final[str] = 'todos/scripts/initial_data.json'
 
 async def initiate_data(session: AsyncSession) -> None:
     with open(INITIAL_DATA_FILE_PATH, 'r') as f:
-        initial_data_dict: dict[str, list[dict]] = json.load(f)
-    priorities: list[Priority] = [
-        Priority(name=p['name']) for p in initial_data_dict['priorities']
-    ]
-    categories: list[Category] = [
-        Category(name=c['name'], created_by_id=None) for c in initial_data_dict['categories']
-    ]
-    session.add_all(priorities)
-    session.add_all(categories)
+        initial_data_dict: dict[str, list[str]] = json.load(f)
+
+    # initiate priorities
+    initial_priorities_names: list[str] = initial_data_dict['priorities_names']
+    priorities_result = await session.execute(
+        (select(Priority).filter(Priority.name.in_(initial_priorities_names)))
+    )
+    priorities_from_db: list[Priority] = priorities_result.scalars().all()
+    if not priorities_from_db:
+        priorities: list[Priority] = [
+            Priority(name=priority_name) for priority_name in initial_priorities_names
+        ]
+        session.add_all(priorities)
+
+    # initiate categories
+    initial_categories_names: list[str] = initial_data_dict['categories_names']
+    categories_result = await session.execute(
+        (select(Category).filter(Category.name.in_(initial_categories_names)))
+    )
+    categories_from_db: list[Category] = categories_result.scalars().all()
+    if not categories_from_db:
+        categories: list[Category] = [
+            Category(name=category_name, created_by_id=None) for category_name in initial_categories_names
+        ]
+        session.add_all(categories)
     await session.commit()
 
 
 async def main() -> None:
+    logger.info('Creating initial data')
     async with Session() as session:
         await initiate_data(session)
+    logger.info('Initial data created')
 
 
 if __name__ == '__main__':
