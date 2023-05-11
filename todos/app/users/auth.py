8d360ff594787d9805033f293d8857f1ad14139e
eladgunders <52347338+eladgunders@users.noreply.github.com>
2023-05-11 11:09:56 +0300

    🐛 fixed url for swagger authorization button (#79)

--- todos/app/users/auth.py
+++ todos/app/users/auth.py
@@ -1,9 +1,13 @@
 from fastapi_users.authentication import AuthenticationBackend, BearerTransport
 
 from app.users.security import get_jwt_strategy
+from app.core.config import get_config
 
 
-bearer_transport = BearerTransport(tokenUrl='auth/login')
+config = get_config()
+
+
+bearer_transport = BearerTransport(tokenUrl=f'{config.API_V1_STR}/auth/login')
 
 auth_backend = AuthenticationBackend(
     name='jwt',
