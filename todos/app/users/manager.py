a9ccfcd2be213fdd2698402dc6e2ac6adb068453
Elad Gunders <gunderselad@gmail.com>
2023-05-12 23:36:02 +0300

    ✨ handling forget password bt sending an email

--- todos/app/users/manager.py
+++ todos/app/users/manager.py
@@ -1,10 +1,13 @@
 import uuid
+from typing import Optional
+from urllib.request import Request
 
 from pydantic import SecretStr
 from fastapi_users import BaseUserManager, UUIDIDMixin
 
 from app.core.config import get_config
 from app.models.tables import User
+from app.utils.emails import send_reset_password_email
 
 
 config = get_config()
@@ -13,3 +16,11 @@ config = get_config()
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_password_token_secret: SecretStr = config.JWT_SECRET_KEY
     verification_token_secret: SecretStr = config.JWT_SECRET_KEY
+
+    async def on_after_forgot_password(
+            self,
+            user: User,
+            token: str,
+            request: Optional[Request] = None
+    ) -> None:
+        send_reset_password_email(email_to=user.email, users_email=user.email, token=token)
---
+++ todos/app/utils/emails.py
@@ -0,0 +1,58 @@
+from typing import Any, Optional
+from pathlib import Path
+import logging
+
+from emails import Message
+from emails.template import JinjaTemplate
+
+from app.core.config import get_config
+
+
+config = get_config()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def send_email(
+    *,
+    email_to: str,
+    environment: Optional[dict[str, Any]],
+    subject_template: str = "",
+    html_template: str = "",
+) -> None:
+    assert config.EMAILS_ENABLED, 'no configuration provided for email variables'
+    if not environment:
+        environment = {}
+    message = Message(
+        subject=JinjaTemplate(subject_template),
+        html=JinjaTemplate(html_template),
+        mail_from=(config.EMAILS_FROM_NAME, config.EMAILS_FROM_EMAIL),
+    )
+    smtp_options = {'host': config.SMTP_HOST, 'port': config.SMTP_PORT}
+    if config.SMTP_TLS:
+        smtp_options['tls'] = True
+    if config.SMTP_USER:
+        smtp_options['user'] = config.SMTP_USER
+    if config.SMTP_PASSWORD:
+        smtp_options['password'] = config.SMTP_PASSWORD.get_secret_value()
+    res = message.send(to=email_to, render=environment, smtp=smtp_options)
+    logger.info('send email result %s', res)
+
+
+def send_reset_password_email(*, email_to: str, users_email: str, token: str) -> None:
+    subject = f'{config.PROJECT_NAME} - Password recovery for user {users_email}'
+    with open(Path(config.EMAIL_TEMPLATES_DIR) / 'reset_password.html', 'r', encoding='utf-8') as f:
+        template_str = f.read()
+    link = f'{config.FRONT_END_BASE_URL}/reset-password?token={token}'
+    send_email(
+        email_to=email_to,
+        subject_template=subject,
+        html_template=template_str,
+        environment={
+            'project_name': config.PROJECT_NAME,
+            'users_email': users_email,
+            'email': email_to,
+            'link': link,
+        }
+    )
