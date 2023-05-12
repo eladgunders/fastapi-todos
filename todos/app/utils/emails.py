ed017273c538a984afabbc4774d177a76b0773fd
Elad Gunders <gunderselad@gmail.com>
2023-05-12 23:48:51 +0300

    💄 cosmetic

--- todos/app/utils/emails.py
+++ todos/app/utils/emails.py
@@ -41,7 +41,7 @@ def send_email(
 
 
 def send_reset_password_email(*, email_to: str, users_email: str, token: str) -> None:
-    subject = f'{config.PROJECT_NAME} - Password recovery for user {users_email}'
+    subject = f'{config.PROJECT_NAME} - Password recovery for email {users_email}'
     with open(Path(config.EMAIL_TEMPLATES_DIR) / 'reset_password.html', 'r', encoding='utf-8') as f:
         template_str = f.read()
     link = f'{config.FRONT_END_BASE_URL}/reset-password?token={token}'
