6f72c62677505e7381d2e3bd7fe852c99ae4ff8a
Elad Gunders <gunderselad@gmail.com>
2023-05-14 15:39:57 +0300

    ⚡ pydantic bad request returns 422

--- todos/app/main.py
+++ todos/app/main.py
@@ -1,12 +1,6 @@
-from urllib.request import Request
-
 import uvicorn
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
-from starlette import status
-from starlette.responses import JSONResponse
 
 from app.api import router
 from app.core.config import get_config
@@ -28,14 +22,6 @@ app.add_middleware(
 
 app.include_router(router)
 
-
-# override the default fastapi HTTP status code (422) with 400 to mark it as a client bad request
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(_: Request, exc: RequestValidationError) -> JSONResponse:
-    return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content=jsonable_encoder({'detail': exc.errors(), 'body': exc.body}),
-    )
 
 if __name__ == '__main__':
     uvicorn.run(app)
