044cf1cf4ed17cb7bf4cd3fc28edffd926359db3
Elad Gunders <gunderselad@gmail.com>
2023-05-15 15:40:45 +0300

    🔥 removed unused code

--- todos/app/api/__init__.py
+++ todos/app/api/__init__.py
@@ -2,10 +2,7 @@ from fastapi import APIRouter
 
 from app.api.health import router as health_router
 from app.api.v1 import router as v1_router
-from app.core.config import get_config
 
-
-config = get_config()
 
 router = APIRouter()
 router.include_router(v1_router)
