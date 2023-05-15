8277586291bbe464edf6a62e69406eca18f30b02
Elad Gunders <gunderselad@gmail.com>
2023-05-15 11:18:01 +0300

    ignored mypy and added a documentation reference

--- todos/app/utils/open_api.py
+++ todos/app/utils/open_api.py
@@ -1,16 +1,19 @@
+from typing import Union
+
 from fastapi_users.openapi import OpenAPIResponseType
 from fastapi_users.router.common import ErrorModel
 
 
 def get_open_api_response(examples_res_details: dict[str, str]) -> OpenAPIResponseType:
-    examples = {}
+    examples: dict[str, dict[str, Union[str, dict[str, str]]]] = {}
     for example, res_detail in examples_res_details.items():
         examples[example] = {
             'summary': example,
             'value': {'detail': res_detail}
         }
     return {
-        'model': ErrorModel,
+        # https://fastapi.tiangolo.com/advanced/additional-responses/#additional-response-with-model
+        'model': ErrorModel,  # type: ignore[dict-item]
         'content': {
             'application/json': {
                 'examples': examples
